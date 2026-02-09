cbde03005ab56470655f0b3ceebe43c0e76256bd
New004 <towelnapkin89alice@proton.me>
2026-02-09 13:43:00 -0500

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,5 +4,7 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-nodejs_version="22.18"
-npm_version="10.9.3"
+nodejs_version="24.13.0"
+npm_version="11.6.2"
+#nodejs_version="22.18"
+#npm_version="10.9.3"
